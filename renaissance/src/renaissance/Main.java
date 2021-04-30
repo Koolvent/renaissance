7712a00c5b66931ae422b31d4cd202ab03355f44
koolvent <koolvent@192.168.184.1>
2021-04-30 22:46:40 +0300

    11

--- renaissance/src/renaissance/Main.java
+++ renaissance/src/renaissance/Main.java
@@ -3,7 +3,7 @@ package renaissance;
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println("Wollo World");
+	System.out.println("Wollo World222222222222222222");
 
 	}
 
